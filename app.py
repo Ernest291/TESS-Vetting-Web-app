9367295ba7f841d2f357c7cbd0e33f978143ed52
Ernest291 <ernestjudet@gmail.com>
2026-06-25 13:41:29 +0800

    added binned overlay

--- app.py
+++ app.py
@@ -54,9 +54,35 @@ if submitted:
                     y=lc_df["flux"],
                     mode="markers",
                     marker=dict(color="deepskyblue", symbol="star", size=6),
+                    name="Flux",
                 )
             )
+            lc_df["bin"] = pd.cut(lc_df["time"], bins=300)
+            binned_lc = lc_df.groupby("bin", observed=False).agg(
+                {"time": "mean", "flux": "mean"}
+            )
+
+            fig1.add_trace(
+                go.Scattergl(
+                    x=binned_lc["time"],
+                    y=binned_lc["flux"],
+                    mode="lines",
+                    line=dict(color="white", width=3),
+                    name="Binned Flux",
+                )
+            )
+            if transit_time != 0:
+                fig1.add_vline(
+                    x=transit_time,
+                    line_dash="dash",
+                    line_color="darkorange",
+                    line_width=3,
+                    annotation_text="t0",
+                    annotation_position="top left",
+                )
+
             fig1.update_layout(
+                title=f"TIC {int(tic_id)} Sector {int(sector)}",
                 xaxis_title="Time(BTJD)",
                 yaxis_title="Normalized Flux",
                 template="plotly_dark",
@@ -66,24 +92,55 @@ if submitted:
         else:
             st.error("No Lightcurve data found.")
 
-        # Fetch BKG_LC data
-        bg_df = get_background_data(tic_id, sector)
-        if bg_df is not None:
-            fig2 = go.Figure()
-            fig2.add_trace(
-                go.Scattergl(
-                    x=bg_df["time"],
-                    y=bg_df["flux"],
-                    mode="markers",
-                    marker=dict(color="darkblue", symbol="star", size=6),
+        # Background Flux section
+        if bg_flux:
+            st.divider()
+            st.write("### Vetting Tests")
+            # Fetch BKG_LC data
+            bg_df = get_background_data(tic_id, sector)
+            if bg_df is not None:
+                fig2 = go.Figure()
+                fig2.add_trace(
+                    go.Scattergl(
+                        x=bg_df["time"],
+                        y=bg_df["flux"],
+                        mode="markers",
+                        marker=dict(color="blue", symbol="star", size=6),
+                        name="Background Flux",
+                    )
                 )
-            )
-            fig2.update_layout(
-                xaxis_title="Time(BTJD)",
-                yaxis_title="Normalized Flux",
-                template="plotly_dark",
-                font=dict(size=18),
-            )
-            st.plotly_chart(fig2, use_container_width=True)
-        else:
-            st.error("No available bg flux data to show.")
+
+                bg_df["bin"] = pd.cut(bg_df["time"], bins=300)
+                binned_bg = bg_df.groupby("bin", observed=False).agg(
+                    {"time": "mean", "flux": "mean"}
+                )
+
+                fig2.add_trace(
+                    go.Scattergl(
+                        x=binned_bg["time"],
+                        y=binned_bg["flux"],
+                        mode="lines",
+                        line=dict(color="white", width=3),
+                        name="Binned BG FLux",
+                    )
+                )
+                if transit_time != 0:
+                    fig2.add_vline(
+                        x=transit_time,
+                        line_dash="dash",
+                        line_color="darkorange",
+                        line_width=3,
+                        annotation_text="t0",
+                        annotation_position="top left",
+                    )
+
+                fig2.update_layout(
+                    title=f"TIC {int(tic_id)} Background Flux",
+                    xaxis_title="Time(BTJD)",
+                    yaxis_title="Unormalized Flux",
+                    template="plotly_dark",
+                    font=dict(size=18),
+                )
+                st.plotly_chart(fig2, use_container_width=True)
+            else:
+                st.error("No available bg flux data to show.")
